68ce6dc6e160ee194f13043ca9cca4b927cb903f
Tommy Chen <tommy351@gmail.com>
2014-05-10 01:42:07 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# [Markdown] renderer
+
+Add support for [Markdown]. This plugin uses [marked] as render engine.
+
+## Install
+
+``` bash
+$ npm install hexo-renderer-marked --save
+```
+
+## Options
+
+You can configure this plugin in `_config.yml`.
+
+``` yaml
+marked:
+  gfm: true
+  pedantic: false
+  sanitize: false
+  tables: true
+  breaks: true
+  smartLists: true
+  smartypants: true
+```
+
+- **gfm** - Enables [GitHub flavored markdown](https://help.github.com/articles/github-flavored-markdown)
+- **pedantic** - Conform to obscure parts of `markdown.pl` as much as possible. Don't fix any of the original markdown bugs or poor behavior.
+- **sanitize** - Sanitize the output. Ignore any HTML that has been input.
+- **tables** - Enable GFM [tables](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#wiki-tables). This option requires the `gfm` option to be true.
+- **breaks** - Enable GFM [line breaks](https://help.github.com/articles/github-flavored-markdown#newlines). This option requires the `gfm` option to be true.
+- **smartLists** - Use smarter list behavior than the original markdown.
+- **smartypants** - Use "smart" typograhic punctuation for things like quotes and dashes.
+
+[Markdown]: http://learnboost.github.io/stylus/
+[marked]: https://github.com/chjj/marked
---
+++ index.js
@@ -0,0 +1,60 @@
+var marked = require('marked'),
+  util = hexo.util,
+  highlight = util.highlight,
+  htmlTag = util.html_tag,
+  format = util.format,
+  stripHtml = format.stripHtml,
+  headingId = {};
+
+var anchorId = function(str){
+  return str
+    .replace(/\s+/g, '_')
+    .replace(/\./g, '-')
+    .replace(/-{2,}/g, '-');
+};
+
+var r = new marked.Renderer();
+
+// Add id attribute to headings
+r.heading = function(text, level){
+  var id = anchorId(stripHtml(text));
+
+  // Add a number after id if repeated
+  if (headingId.hasOwnProperty(id)){
+    id += '-' + headingId[id];
+    headingId[text]++;
+  } else {
+    headingId[id] = 1;
+  }
+
+  return htmlTag('h' + level, {id: id}, text) + '\n';
+};
+
+marked.setOptions({
+  gfm: true,
+  pedantic: false,
+  sanitize: false,
+  tables: true,
+  breaks: true,
+  smartLists: true,
+  smartypants: true,
+  renderer: r,
+  langPrefix: '',
+  highlight: function(code, lang){
+    return highlight(code, {lang: lang, gutter: false, wrap: false});
+  }
+});
+
+var renderer = function(data, options){
+  headingId = {};
+
+  return marked(data.text, options);
+};
+
+hexo.extend.renderer.register('md', 'html', renderer, true);
+hexo.extend.renderer.register('markdown', 'html', renderer, true);
+hexo.extend.renderer.register('mkd', 'html', renderer, true);
+hexo.extend.renderer.register('mkdn', 'html', renderer, true);
+hexo.extend.renderer.register('mdwn', 'html', renderer, true);
+hexo.extend.renderer.register('mdtxt', 'html', renderer, true);
+hexo.extend.renderer.register('mdtext', 'html', renderer, true);
