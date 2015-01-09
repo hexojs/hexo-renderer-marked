47950d4301a10a3d26d40aaa798a408f9381ef90
Tommy Chen <tommy351@gmail.com>
2015-01-09 22:01:45 +0800

    Define default config when loading the plugin

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var renderer = require('./lib/renderer');
+var assign = require('object-assign');
+
+hexo.config.marked = assign({
+  gfm: true,
+  pedantic: false,
+  sanitize: false,
+  tables: true,
+  breaks: true,
+  smartLists: true,
+  smartypants: true
+}, hexo.config.marked);
 
 hexo.extend.renderer.register('md', 'html', renderer, true);
 hexo.extend.renderer.register('markdown', 'html', renderer, true);
