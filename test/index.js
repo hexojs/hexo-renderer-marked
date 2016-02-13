951baa05711c91924a4033f88374523e336e2af7
Tommy Chen <tommy351@gmail.com>
2016-02-13 20:29:13 +0800

    Use slugize method of hexo-util to fix Chinese headers can't be handle properly

--- test/index.js
+++ test/index.js
@@ -30,9 +30,9 @@ describe('Marked renderer', function() {
     var result = r({text: body});
 
     result.should.eql([
-        '<h1 id="Hello_world"><a href="#Hello_world" class="headerlink" title="Hello world"></a>Hello world</h1>',
+        '<h1 id="Hello-world"><a href="#Hello-world" class="headerlink" title="Hello world"></a>Hello world</h1>',
         '<pre><code>' + util.highlight(code, {gutter: false, wrap: false}) + '\n</code></pre>',
-        '<h2 id="Hello_world-1"><a href="#Hello_world-1" class="headerlink" title="Hello world"></a>Hello world</h2>',
+        '<h2 id="Hello-world-1"><a href="#Hello-world-1" class="headerlink" title="Hello world"></a>Hello world</h2>',
         '<p>hello</p>'
       ].join('') + '\n');
   });
@@ -50,5 +50,12 @@ describe('Marked renderer', function() {
       '<h2 id="hexo-server"><a href="#hexo-server" class="headerlink" title="hexo-server"></a>',
       '<a href="https://github.com/hexojs/hexo-server">hexo-server</a></h2>'
     ].join(''));
+  });
+
+  it('should handle chinese headers properly', function() {
+    var body = '# 中文';
+    var result = r({text: body});
+
+    result.should.eql('<h1 id="中文"><a href="#中文" class="headerlink" title="中文"></a>中文</h1>');
   });
 });
