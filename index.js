d28dc94344f4e87d230f8eca521155f8e964a3cc
SukkaW <isukkaw@gmail.com>
2020-04-21 21:04:38 +0800

    revert: feat: promisify

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ hexo.config.marked = Object.assign({
   }
 }, hexo.config.marked);
 
-hexo.extend.renderer.register('md', 'html', renderer);
-hexo.extend.renderer.register('markdown', 'html', renderer);
-hexo.extend.renderer.register('mkd', 'html', renderer);
-hexo.extend.renderer.register('mkdn', 'html', renderer);
-hexo.extend.renderer.register('mdwn', 'html', renderer);
-hexo.extend.renderer.register('mdtxt', 'html', renderer);
-hexo.extend.renderer.register('mdtext', 'html', renderer);
+hexo.extend.renderer.register('md', 'html', renderer, true);
+hexo.extend.renderer.register('markdown', 'html', renderer, true);
+hexo.extend.renderer.register('mkd', 'html', renderer, true);
+hexo.extend.renderer.register('mkdn', 'html', renderer, true);
+hexo.extend.renderer.register('mdwn', 'html', renderer, true);
+hexo.extend.renderer.register('mdtxt', 'html', renderer, true);
+hexo.extend.renderer.register('mdtext', 'html', renderer, true);
